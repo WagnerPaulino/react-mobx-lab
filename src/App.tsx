eef5eeeb1859d0601f73350412fffa24a905799e
Wagner Paulino <wagnersantos9778@gmail.com>
2020-02-27 17:51:34 -0300

    ajuste

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import React, { useContext } from 'react';
+import React, { useContext, createContext } from 'react';
 import './App.css';
-
-import { createContext } from 'react';
 import { TodoList } from "./todo-list";
 import { TodoNewComponent } from './components/todo-new.component';
 import { TodoListComponent } from './components/todo-list.component';
@@ -10,9 +8,9 @@ export const StoreContext = createContext<TodoList>({} as TodoList);
 export const StoreProvider = StoreContext.Provider;
 
 const todoList = new TodoList([
-  'Should Starting Writing in React',
-  'Should Learn MobX',
-  'Should Watch Once Piece :)'
+  'Começar a aprender React',
+  'Começar a aprender MobX',
+  'Começar a assistir Nanatsu no Taizai :)'
 ]);
 
 export const useStore = (): TodoList => useContext(StoreContext);
